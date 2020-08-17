2efe8cc98d6731c6c45b994f71e19ec3b202738d
h-sakano <h.sakano@world-bit.co.jp>
2020-08-17 11:18:42 +0900

    pageSIzeOptionsを更新

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -24,6 +24,7 @@ const PostsList: React.FC<Props> = ({ data }) => {
       pagination={{
         onShowSizeChange: handleShowSizeChange,
         pageSize,
+        pageSizeOptions: ['5', '10', '20', '50', '100'],
       }}
       dataSource={data.allMicrocmsPosts.edges}
       renderItem={(item) => (
