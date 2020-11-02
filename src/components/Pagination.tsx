dfdcbaf7b530623ba80f530750ef012c01c28d07
Hiroki Sakano <h.sakano26@gmail.com>
2020-11-02 18:37:37 +0900

    Paginationの修正

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -155,15 +155,16 @@ const Pagination: React.FC<PaginationProps> = ({
                       ...
                     </span>
                   )}
-                  {pageCount >= sidesNumMd * 2 &&
-                    (p === 2 || p === pageCount - sidesNumMd + 1) && (
-                      <Link
-                        to={`${trimmedbaseUrl}/${p}`}
-                        className="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
-                      >
-                        {p}
-                      </Link>
-                    )}
+                  {((pageCount >= 3 && p === 2) ||
+                    (pageCount >= sidesNumMd * 2 &&
+                      p === pageCount - sidesNumMd + 1)) && (
+                    <Link
+                      to={`${trimmedbaseUrl}/${p}`}
+                      className="hidden md:inline-flex -ml-px relative items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+                    >
+                      {p}
+                    </Link>
+                  )}
                   {(p < 2 || p > pageCount - sidesNumMd + 1) && (
                     <Link
                       to={
